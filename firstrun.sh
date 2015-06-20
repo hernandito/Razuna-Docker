13db4148d98d9c60fa09975a2f46759c19d80052
hernandito <archi@inch.com>
2015-06-20 15:19:32 -0400

    Install FFMPEG w/ startup script

--- firstrun.sh
+++ firstrun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if ResourceSpace exists. If not, copy from /home
-if [ -f /opt/razuna/tomcat/startup.sh ]; then
+if [ -f /opt/razuna/tomcat/bin/startup.sh ]; then
   echo "Using existing Razuna install."
 else
   echo "Copying Razuna into /opt folder."
@@ -10,6 +10,17 @@ else
  fi
  
  
-chmod -R 777 /opt/razuna/tomcat/bin/
-cd /opt/razuna/tomcat/bin/
-#./startup.sh
+ # Check if ffmpeg is installed
+ if [ -f /usr/bin/ffmpeg ]; then
+		echo "FFMpeg is already installed... doing nothing."
+ else		
+		add-apt-repository ppa:mc3man/trusty-media -y
+		apt-get update
+		apt-get update
+		apt-get install -y ffmpeg
+  fi
+ 
+# Starting Razuna
+	chmod -R 777 /opt/razuna/tomcat/bin/
+	cd /opt/razuna/tomcat/bin/
+	./startup.sh
